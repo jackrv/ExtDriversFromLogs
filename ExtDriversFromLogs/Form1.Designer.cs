610a67d2f3ed11774aedf21d372f1049e52a6d0f
jackrv <jackrv89@gmail.com>
2015-02-26 15:31:54 +0200

    fist

---
+++ ExtDriversFromLogs/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ExtDriversFromLogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.вчераToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.другаяДатаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openButton1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(32, 19);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(0, -3);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(470, 266);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.DoubleClick += new System.EventHandler(this.listView1_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "№";
+            this.columnHeader1.Width = 30;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ник";
+            this.columnHeader2.Width = 298;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Tag = "";
+            this.columnHeader3.Text = "SteamID";
+            this.columnHeader3.Width = 120;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(480, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выйти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(474, 87);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(95, 20);
+            this.nameBox.TabIndex = 5;
+            this.nameBox.TextChanged += new System.EventHandler(this.nameBox_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.вчераToolStripMenuItem,
+            this.другаяДатаToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(115, 48);
+            // 
+            // вчераToolStripMenuItem
+            // 
+            this.вчераToolStripMenuItem.Name = "вчераToolStripMenuItem";
+            this.вчераToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.вчераToolStripMenuItem.Text = "Вчерашний";
+            this.вчераToolStripMenuItem.Click += new System.EventHandler(this.вчераToolStripMenuItem_Click);
+            // 
+            // другаяДатаToolStripMenuItem
+            // 
+            this.другаяДатаToolStripMenuItem.Name = "другаяДатаToolStripMenuItem";
+            this.другаяДатаToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.другаяДатаToolStripMenuItem.Text = "Другая дата";
+            this.другаяДатаToolStripMenuItem.Click += new System.EventHandler(this.другаяДатаToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.openButton1);
+            this.groupBox1.Location = new System.Drawing.Point(474, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(116, 64);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Открыть лог";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "0 / 0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(16, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "▼";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // openButton1
+            // 
+            this.openButton1.Location = new System.Drawing.Point(6, 19);
+            this.openButton1.Name = "openButton1";
+            this.openButton1.Size = new System.Drawing.Size(93, 23);
+            this.openButton1.TabIndex = 3;
+            this.openButton1.Text = "Сегодняшний";
+            this.openButton1.UseVisualStyleBackColor = true;
+            this.openButton1.Click += new System.EventHandler(this.openButton1_Click_1);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(575, 90);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.checkBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(594, 263);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "ExtDfL (MP)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ToolStripMenuItem вчераToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem другаяДатаToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button openButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ ExtDriversFromLogs/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace ExtDriversFromLogs
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> drivers = new Dictionary<string, string>();
+        public void openFile(string path)
+        {
+            listView1.Items.Clear();
+            drivers.Clear();
+            string t_line = string.Empty;
+
+            if (File.Exists(path))
+            {
+                string lines = File.ReadAllText(path, Encoding.Default);
+
+                string pattern = @"\[.*\]\sSpawning\sGameTrailer\s\((?<name>.*?)\s\|\s(?<id>7656119[0-9]{10}$?)\)";
+
+                Regex rgx = new Regex(pattern);
+                foreach (Match match in rgx.Matches(lines))
+                {
+                    if (!drivers.ContainsKey(match.Groups["id"].Value))
+                    {
+                        drivers.Add(match.Groups["id"].Value, match.Groups["name"].Value);
+                    }
+                }
+                load_list(nameBox.Text);
+                //MessageBox.Show("Найдено " + drivers.Count + " водителей!", "Информация", MessageBoxButtons.OK);
+            }
+            else
+                MessageBox.Show("Файл логов " + path + " не обнаружен!", "Ошибка открытия", MessageBoxButtons.OK);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            contextMenuStrip1.Show(button2, new Point(0, button2.Height));
+        }
+
+        private void openButton1_Click_1(object sender, EventArgs e)
+        {
+            string personal_dir = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string date = DateTime.Now.Date.ToString("dd_MM_yyyy");
+            string path = personal_dir + "\\ETS2MP\\logs\\log_spawning_" + date + ".log";
+            openFile(path);
+        }
+
+        private void вчераToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string personal_dir = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            string date = DateTime.Now.AddDays(-1).ToString("dd_MM_yyyy");
+            string path = personal_dir + "\\ETS2MP\\logs\\log_spawning_" + date + ".log";
+            openFile(path);
+        }
+
+        private void другаяДатаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string md = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog1.InitialDirectory = md + "\\ETS2MP\\logs";
+            openFileDialog1.Filter = "Log files|log_spawning_*.log";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string personal_dir = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                    string path = openFileDialog1.FileName;
+                    openFile(path);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось загрузить файл: " + ex.Message);
+                }
+            }
+        }
+
+        private void load_list(string tag)
+        {
+            int i = 0;
+            foreach(KeyValuePair<string, string> k in drivers)
+            {
+                string value = k.Value;
+                if (!checkBox1.Checked)
+                {
+                    tag = tag.ToLowerInvariant();
+                    value = k.Value.ToLowerInvariant();
+                }
+
+                if (value.Contains(tag))
+                {
+                    i++;
+                    listView1.Items.Add(new ListViewItem(new[] { i.ToString(), k.Value, k.Key }));
+                }
+                
+            }
+            label1.Text = i + " / " + drivers.Count;
+        }
+
+        private void nameBox_TextChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            load_list(nameBox.Text);
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            Clipboard.SetText(listView1.FocusedItem.SubItems[1].Text + " " + listView1.FocusedItem.SubItems[2].Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey readKey = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("software\\extdfl");
+                string tag = (string)readKey.GetValue("tag");
+                string registry = (string)readKey.GetValue("registry");
+                int pos_x = (int)readKey.GetValue("pos_x");
+                int pos_y = (int)readKey.GetValue("pos_y");
+                readKey.Close();
+                nameBox.Text = tag;
+                if (registry == "True")
+                    checkBox1.Checked = true;
+                else
+                    checkBox1.Checked = false;
+
+                this.Location = new System.Drawing.Point(pos_x, pos_y);
+            }
+            catch { }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Microsoft.Win32.RegistryKey saveKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("software\\extdfl");
+                saveKey.SetValue("tag", nameBox.Text);
+                saveKey.SetValue("registry", checkBox1.Checked);
+                saveKey.SetValue("pos_x", this.Location.X);
+                saveKey.SetValue("pos_y", this.Location.Y);
+                saveKey.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Не удалось загрузить данные из реестра: " + ex.Message);
+            }
+        }
+
+        private void checkBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            toolTip1.Show("С учетом регистра", this.checkBox1);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            load_list(nameBox.Text);
+        }
+    }
+}
